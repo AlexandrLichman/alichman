1fbb9ad21a6a77d34ff56d6af4f6c84c0451a2d4
AlexandrLichman <alexlich.htc@gmail.com>
2017-02-19 20:23:36 +0200

    _

--- src/ru/job4j/Calculate.java
+++ src/ru/job4j/Calculate.java
@@ -1,7 +1,7 @@
-package src.ru.job4j;
+п»їpackage src.ru.job4j;
 
 public class Calculate {
 	public static void main(String[] args) {
-		System.out.println("Привет мир");
+		System.out.println("РџСЂРёРІРµС‚ РјРёСЂ");
 	}
 }
