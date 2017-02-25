ef51216aa9583f9f79012b337c13da60cfce2609
AlexandrLichman <alexlich.htc@gmail.com>
2017-02-25 20:07:41 +0200

    adited

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
-package ru.job4j;
+п»їpackage ru.job4j;
+
+/**
+ * Calculate slass.
+ * @author A. lichman
+ * @since 25.02.2017
+ */
 
 public class Calculate {
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
-		System.out.println("Привет мир");
+		System.out.println("РџСЂРёРІРµС‚ РјРёСЂ");
 	}
 }
